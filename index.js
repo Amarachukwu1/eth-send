334a059c767bbb2c923eb80be4dacbbc4faaa66e
Amarachukwu1 <97677585+Amarachukwu1@users.noreply.github.com>
2022-01-16 12:46:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const Web3 = require('web3')
-const privateKeyToAddress = require('ethereum-private-key-to-address')
-const PrivateKeyProvider = require('truffle-privatekey-provider')
 const chalk = require('chalk')
 
 async function send (config) {
-  const privateKey = (config.from || '').replace(/^0x/, '')
   const to = config.to
   let amount = config.amount || 0
   let value = config.value || 0
@@ -17,7 +14,7 @@ async function send (config) {
     providerUri = 'http://localhost:8545'
   }
 
-  const provider = new PrivateKeyProvider(privateKey, providerUri)
+  
   const web3 = new Web3(provider)
 
   if (value && amount) {
@@ -29,11 +26,6 @@ async function send (config) {
   } else {
     value = web3.utils.toWei(amount, 'ether')
   }
-
-  const from = privateKeyToAddress(privateKey)
-  const data = config.data || '0x'
-  const gas = config.gas || 21000
-  const gasPrice = config.gasPrice || await web3.eth.getGasPrice()
 
   if (config.log) {
     console.log(chalk.yellow('sending transaction:'))
